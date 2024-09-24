0e577fe80a4aef53dc357b7d27e862403b31d234
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2024-09-24 17:49:24 +0200

    frist commit

---
+++ GameZone/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using GameZone.Models;
+using GameZone.Models.viewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly AppDbcontext _context;
+
+        public GamesController(AppDbcontext context)
+        {
+            _context = context;
+        }
+   
+        
+    }
+}
+
---
+++ GameZone/Program.cs
@@ -0,0 +1,36 @@
+using GameZone.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbcontext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BlogaatConnectjjhionStrings")));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
